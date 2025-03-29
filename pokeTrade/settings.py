0d941170d0bf19fbf4eee75f2e68f1552033dfe3
Sam <skarki48@myseneca.ca>
2025-03-29 18:49:25 -0400

    Add Django Debug Toolbar for enhanced debugging capabilities

--- pokeTrade/settings.py
+++ pokeTrade/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_toolbar'
 ]
 
 MIDDLEWARE = [
@@ -46,6 +47,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+INTERNAL_IPS = [
+    "127.0.0.1",
 ]
 
 ROOT_URLCONF = 'pokeTrade.urls'
